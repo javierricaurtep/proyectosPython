ce732ac5769154d2741813ac7f66ef95c4c9543b
Javier Ricaurte <javierricaurtep@gmail.com>
2022-06-27 12:45:18 -0500

    Crear carpetas para organizar cursos

--- condicionales.py
+++
@@ -1,5 +0,0 @@
-edad = int(input("Por favor escribe tu edad: "))
-if edad > 17:
-    print("Eres mayor de edad")
-else:
-    print("Eres menor de edad")
--- for.py
+++
@@ -1,6 +0,0 @@
-def run():
-    for contador in range(1000):
-        print(contador) 
-
-if __name__ == "__main__":
-    run()
